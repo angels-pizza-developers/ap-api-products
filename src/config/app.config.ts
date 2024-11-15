9aa60519a3b129a568e0dcc5b615b658c2083a3d
erwin-io <erwinramirez220@gmail.com>
2024-11-15 09:05:20 +0800

    temporary

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,9 +2,12 @@ import secretManagerConfig from "./secret-manager.config";
 export function createConfig(): any {
   const env = process.env.NODE_ENV || "development";
   if (env && env !== "development") {
+    // return {
+    //   load: [secretManagerConfig],
+    // };
     return {
-      load: [secretManagerConfig],
-    };
+      envFilePath: [".env"],
+    }
   } else {
     return {
       envFilePath: [".env", `.env.${process.env.NODE_ENV || "development"}`],
