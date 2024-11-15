a42b63a0962f3d45de3dc68bc7883a330734e44d
erwin-io <erwinramirez220@gmail.com>
2024-11-15 09:31:29 +0800

    temp custom swagger ui for serverless deployment

--- src/main.ts
+++ src/main.ts
@@ -37,9 +37,21 @@ async function bootstrap() {
   // the next two lines did the trick
   app.use(bodyParser.json({ limit: "50mb" }));
   app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup("swagger", app, document, {
     swaggerOptions: { defaultModelsExpandDepth: -1 },
+    //for serverless deployment
+    customfavIcon: "https://avatars0.githubusercontent.com/u/7658037?v=3&s=200",
+    customJs: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js",
+    ],
+    customCssUrl: [
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css",
+    ],
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   // Apply the response interceptor globally
